c6db260ffeafecc3bd9ca9f2b9fe4a8ab498de52
Jamil18474 <jamil.zahrane@gmail.com>
2023-06-21 21:09:58 +0200

    Gestion de la connexion des utilisateurs
    Gestion de la connexion des utilisateurs à l'aide d'une requête de base de données.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  utilisateursCollection: AngularFirestoreCollection<any> | null = null;
+  utilisateurs: Observable<any[]> | null = null;
+  telephone: string = '';
+  code: string = '';
+  message: string = '';
+  
+  constructor(public db: AngularFirestore) { }
 
+  ngOnInit() {
+    this.utilisateursCollection = this.db.collection('utilisateurs');
+    this.utilisateurs = this.utilisateursCollection.valueChanges();
+  }
+
+  // on vérifie les informations saisies pour se connecter
+  verifierInfos(telephone: string, code:string) {
+    // on recherche dans la collection des utilisateurs si le téléphone et le code appartient à un utilisateur
+    this.db.collection('utilisateurs', ref =>
+      ref.where('telephone', '==', telephone).where('code', '==', code)).get().subscribe((querySnapshot) => {
+      // on vérifie qu'un document a été trouvé
+      if (querySnapshot.docs.length === 0) {
+        this.message = 'Erreur de connexion, vérifier vos informations de connexion.';
+        return;
+      }
+      else {
+        this.message = 'Vous êtes bien connecté!';
+      }
+    });  
+  }
 }
--- src/app/singup/singup.component.ts
+++ src/app/singup/singup.component.ts
@@ -1,20 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
-import { ActivatedRoute } from "@angular/router";
 
-
-
-interface Utilisateur {
-  id: string;
-  nom: string;
-  prenom: string;
-  telephone: string;
-  email: string;
-  code: string;
-  typeUtilisateur: string;
-  
-}
 @Component({
   selector: 'app-singup',
   templateUrl: './singup.component.html',
@@ -23,11 +10,9 @@ interface Utilisateur {
 
 
 export class SingupComponent{
-
+  // propriétés de la classe
   utilisateursCollection: AngularFirestoreCollection<any> | null = null;
   utilisateurs: Observable<any[]> | null = null;
-  id: string = "";
-  
   nom: string = '';
   prenom: string = '';
   telephone: string = '';
@@ -43,35 +28,28 @@ export class SingupComponent{
     this.utilisateurs = this.utilisateursCollection.valueChanges();
   }
   
-
+  // Création d'un utilisateur
   creerUtilisateur() {
-    
-
-  const utilisateur = {
-    
-    nom: this.nom,
-    prenom: this.prenom,
-    telephone: this.telephone,
-    email: this.email,
-    code: this.code,
-    typeUtilisateur: this.typeUtilisateur,
-
-  };
-  if (!this.typeUtilisateur || !this.nom || !this.prenom || !this.telephone || !this.code) {
-    this.message = 'Veuillez remplir tous les champs obligatoires.';
-    return;
-  }
-
-  if (this.utilisateursCollection) {
-    this.utilisateursCollection.add(utilisateur);
-    this.message = 'L\'utilisateur a été créé avec succès.';
-    
-  } else {
-    console.error("Erreur dans la requête.");
-  }
-
-    
-
-    
-  }
+    // on construit un utilisateur avec ses informatiions
+    const utilisateur = {
+      nom: this.nom,
+      prenom: this.prenom,
+      telephone: this.telephone,
+      email: this.email,
+      code: this.code,
+      typeUtilisateur: this.typeUtilisateur,
+    };
+    // on vérifie si les champs ne sont pas vides à l'exception de l'email qui est facultatif
+    if (!this.typeUtilisateur || !this.nom || !this.prenom || !this.telephone || !this.code) {
+      this.message = 'Veuillez remplir tous les champs obligatoires.';
+      return;
+    }
+    // si la collection des utilisateurs existe, on ajoute l'utilisateur
+    if (this.utilisateursCollection) {
+      this.utilisateursCollection.add(utilisateur);
+      this.message = 'L\'utilisateur a été créé avec succès.';  
+    } else {
+      console.error("Erreur dans la requête.");
+    }
+  } 
 }
