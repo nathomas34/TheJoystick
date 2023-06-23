07fd329a37c5640ca9b3468086d29a6dc93e1841
Jamil18474 <jamil.zahrane@gmail.com>
2023-06-23 23:54:03 +0200

    Création de jeux dans une autre page
    Création de jeux et redirection vers la page d'accueil.

--- src/app/formcrea/formcrea.component.ts
+++ src/app/formcrea/formcrea.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
+interface Jeu {
+  nom: string;
+  description: string;
+  image: string;
+  createur: string;
+  votes: string[];
+}
 
 @Component({
   selector: 'app-formcrea',
   templateUrl: './formcrea.component.html',
-  styleUrls: ['./formcrea.component.css']
+  styleUrls: ['./formcrea.component.css'],
 })
-export class FormcreaComponent {
+export class FormcreaComponent implements OnInit {
+  // propriétés de la classe
+  jeuxCollection: AngularFirestoreCollection<Jeu> | null = null;
+  jeux: Observable<Jeu[]> | null = null;
+  nom: string = '';
+  description: string = '';
+  image: string = '';
+  votes: string[] = [''];
+  createur: string = '';
+  message: string = '';
 
+  constructor(public db: AngularFirestore) {}
+
+  ngOnInit() {
+    // on récupère la liste des jeux
+    this.jeuxCollection = this.db.collection('jeux');
+    this.jeux = this.jeuxCollection.valueChanges();
+  }
+
+  // Création d'un jeu
+  creerJeu() {
+    // on construit un jeu avec ses informatiions
+    const jeu = {
+      nom: this.nom,
+      description: this.description,
+      image: this.image,
+      createur: this.createur,
+      votes: this.votes,
+    };
+    // on vérifie si les champs ne sont pas vides
+    if (!this.nom || !this.description || !this.image || !this.createur) {
+      this.message = 'Veuillez remplir tous les champs obligatoires.';
+      return;
+    }
+    // si la collection des jeux existe, on ajoute le jeu
+    if (this.jeuxCollection) {
+      this.jeuxCollection.add(jeu);
+      this.message = 'Le jeu a été créé avec succès.';
+    } else {
+      this.message = 'Erreur dans la requête.';
+    }
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 
 interface Utilisateur {
-  nom:string;
-  prenom : string;
+  nom: string;
+  prenom: string;
   telephone: string;
   email: string;
   code: string;
@@ -13,17 +16,17 @@ interface Utilisateur {
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   // propriétés de la classe
   utilisateursCollection: AngularFirestoreCollection<Utilisateur> | null = null;
   utilisateurs: Observable<Utilisateur[]> | null = null;
   telephone: string = '';
   code: string = '';
   message: string = '';
-  
-  constructor(public db: AngularFirestore) { }
+
+  constructor(public db: AngularFirestore) {}
 
   ngOnInit() {
     // on récupère la liste des utilisateurs
@@ -32,21 +35,26 @@ export class LoginComponent implements OnInit{
   }
 
   // on vérifie les informations saisies pour se connecter
-  logIn(telephone: string, code:string) {
+  logIn(telephone: string, code: string) {
     // on recherche dans la collection des utilisateurs si le téléphone et le code appartient à un utilisateur
-    this.db.collection('utilisateurs', ref =>
-      ref.where('telephone', '==', telephone).where('code', '==', code)).get().subscribe((querySnapshot) => {
-      // on vérifie qu'un document a été trouvé
-      if (querySnapshot.docs.length === 0) {
-        this.message = 'Erreur de connexion, vérifier vos informations de connexion.';
-        return;
-      } else {
-        //on récupére l'id du document trouvé
-        const utilisateurId = querySnapshot.docs[0].id;
-        this.message = 'Vous êtes bien connecté!';
-        //on stocke l'id de l'utilisateur avec localStorage afin de persister cette donnée
-        localStorage.setItem('utilisateurId', utilisateurId);
-      }
-    });  
+    this.db
+      .collection('utilisateurs', (ref) =>
+        ref.where('telephone', '==', telephone).where('code', '==', code)
+      )
+      .get()
+      .subscribe((querySnapshot) => {
+        // on vérifie qu'un document a été trouvé
+        if (querySnapshot.docs.length === 0) {
+          this.message =
+            'Erreur de connexion, vérifier vos informations de connexion.';
+          return;
+        } else {
+          //on récupére l'id du document trouvé
+          const utilisateurId = querySnapshot.docs[0].id;
+          this.message = 'Vous êtes bien connecté!';
+          //on stocke l'id de l'utilisateur avec localStorage afin de persister cette donnée
+          localStorage.setItem('utilisateurId', utilisateurId);
+        }
+      });
   }
 }
